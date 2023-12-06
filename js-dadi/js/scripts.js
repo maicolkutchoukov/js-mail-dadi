ef0022fde3ae673c167eb3993ea3bb16451907b1
maicolkutchoukov <maicol.j.95@gmail.com>
2023-12-06 15:01:13 +0100

    Completato esercizio dadi

--- js-dadi/js/scripts.js
+++ js-dadi/js/scripts.js
@@ -1,1 +1,26 @@
-console.log('Template html-css-js');
+/*
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+*/
+
+const pcNumber = Math.floor(Math.random() * 6 + 1);
+console.log('Numero del pc', pcNumber, typeof pcNumber);
+const userNumber = Math.floor(Math.random() * 6 + 1);
+console.log('Numero dell\'utente', userNumber, typeof userNumber);
+
+const dado1 = document.querySelector('.dado-pc');
+const dado2 = document.querySelector('.dado-utente')
+dado1.innerHTML = pcNumber;
+dado2.innerHTML = userNumber;
+const risultato = document.querySelector('p');
+if (pcNumber > userNumber){
+    risultato.innerHTML = 'Ha vinto il PC';
+}
+else if (pcNumber < userNumber) {
+    risultato.innerHTML = 'Hai Vinto tu';
+
+}
+else {
+    risultato.innerHTML = 'Pareggio';
+
+}
