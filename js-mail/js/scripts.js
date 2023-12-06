32717b06e1ecb2ab6b8a637948ad0ec2476555b8
maicolkutchoukov <maicol.j.95@gmail.com>
2023-12-06 14:16:16 +0100

    Completato esercizio mail

--- js-mail/js/scripts.js
+++ js-mail/js/scripts.js
@@ -4,3 +4,23 @@ controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull'esito del controllo.
 */
 
+const arrayMail = ['alessio@gmail.com', 'mauro@gmail.com', 'marco@gmail.com', 'ciao@gmail.com','maicol.j.95@gmail.com'];
+console.log('email', arrayMail, arrayMail.length, typeof arrayMail)
+
+let email = prompt('Inserisci la tua email');
+let risultato = ""
+
+for (let i = 0; i < arrayMail.length; i++) {
+    if (arrayMail[i] == email){
+        risultato = 1
+    }
+}
+
+if (risultato != ""){
+    alert('Accesso consentito');
+    console.log('Puoi accedere')
+}
+else {
+    alert('Accesso negato');
+    console.log('Non puoi accedere')
+}
